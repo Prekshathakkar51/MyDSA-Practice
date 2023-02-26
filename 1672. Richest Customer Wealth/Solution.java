5992e04afd8550a97c74c68a94de7cc12c70d716
Preksha Thakkar <95307102+Prekshathakkar51@users.noreply.github.com>
2023-02-26 23:21:33 +0530

    code

---
+++ 1672. Richest Customer Wealth/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        // person = row
+        // account = col
+        int ans  = Integer.MIN_VALUE;
+        for(int[] ints : accounts ){
+            // when you start a new col, take a new sum for that row
+            int sum = 0;
+            for(int anInt : ints ){
+                sum += anInt;
+            }
+
+            // now we have sum of accounts of person
+            //check overall value
+
+            if(sum>ans){
+                ans = sum;
+            }
+        }
+        return ans;
+    
